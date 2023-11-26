464733967a430411b0966187a4cc569eac8ff5c6
Moch Aldrian <mhmdsaldrian96@gmail.com>
2023-11-26 18:37:50 +0700

    First Commit

---
+++ src/views/components/AuthorsTable.vue
@@ -0,0 +1,311 @@
+<template>
+  <div class="card">
+    <div class="card-header pb-0">
+      <h6>Tabel Pengaduan</h6>
+    </div>
+    <div class="card-body px-0 pt-0 pb-2">
+      <div class="table-responsive p-0">
+        <table class="table align-items-center mb-0">
+          <thead>
+            <tr>
+              <th
+                class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7"
+              >
+                Judul Pengaduan
+              </th>
+              <th
+                class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2"
+              >
+                Saran Solusi
+              </th>
+              <th
+                class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7"
+              >
+                Status Pengaduan
+              </th>
+              <!-- <th
+                class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7"
+              >
+                Saran Solusi
+              </th> -->
+              <th class="text-secondary opacity-7"></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <div class="d-flex px-2 py-1">
+                  <div>
+                    <img
+                      src="../../assets/img/high.png"
+                      class="avatar avatar-sm me-3"
+                      alt="user1"
+                    />
+                  </div>
+                  <div class="d-flex flex-column justify-content-center">
+                    <h6 class="mb-0 text-sm">Jalan Rusak</h6>
+                    <p class="text-xs text-secondary mb-0">
+                      jalan ini sudah 1 bulan rusak karena sering.....
+                    </p>
+                  </div>
+                </div>
+              </td>
+              <td>
+                <p class="text-xs font-weight-bold mb-0">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  <br />deleniti molestiae doloribus voluptates natus eos ex
+                  quisquam...
+                </p>
+                <!-- <p class="text-xs text-secondary mb-0">Organization</p> -->
+              </td>
+              <td class="align-middle text-center text-sm">
+                <span class="badge badge-sm bg-gradient-success">Selesai</span>
+              </td>
+              <!-- <td class="align-middle text-center">
+                <span class="text-secondary text-xs font-weight-bold"
+                  >23/04/18</span
+                >
+              </td> -->
+              <td class="align-middle">
+                <a
+                  href="javascript:;"
+                  class="text-secondary font-weight-bold text-xs text-center"
+                  data-toggle="tooltip"
+                  data-original-title="Detail user"
+                  >Detail</a
+                >
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <div class="d-flex px-2 py-1">
+                  <div>
+                    <img
+                      src="../../assets/img/high.png"
+                      class="avatar avatar-sm me-3"
+                      alt="user2"
+                    />
+                  </div>
+                  <div class="d-flex flex-column justify-content-center">
+                    <h6 class="mb-0 text-sm">Jalan Rusak</h6>
+                    <p class="text-xs text-secondary mb-0">
+                      jalan ini sudah 1 bulan rusak karena sering.....
+                    </p>
+                  </div>
+                </div>
+              </td>
+              <td>
+                <p class="text-xs font-weight-bold mb-0">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  <br />deleniti molestiae doloribus voluptates natus eos ex
+                  quisquam...
+                </p>
+              </td>
+              <td class="align-middle text-center text-sm">
+                <span class="badge badge-sm bg-gradient-secondary"
+                  >Diproses</span
+                >
+              </td>
+              <!-- <td class="align-middle text-center">
+                <span class="text-secondary text-xs font-weight-bold"
+                  >11/01/19</span
+                >
+              </td> -->
+              <td class="align-middle">
+                <a
+                  href="javascript:;"
+                  class="text-secondary font-weight-bold text-xs"
+                  data-toggle="tooltip"
+                  data-original-title="Detail user"
+                  >Detail</a
+                >
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <div class="d-flex px-2 py-1">
+                  <div>
+                    <img
+                      src="../../assets/img/high.png"
+                      class="avatar avatar-sm me-3"
+                      alt="user3"
+                    />
+                  </div>
+                  <div class="d-flex flex-column justify-content-center">
+                    <h6 class="mb-0 text-sm">Jalan Rusak</h6>
+                    <p class="text-xs text-secondary mb-0">
+                      jalan ini sudah 1 bulan rusak karena sering.....
+                    </p>
+                  </div>
+                </div>
+              </td>
+              <td>
+                <p class="text-xs font-weight-bold mb-0">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  <br />deleniti molestiae doloribus voluptates natus eos ex
+                  quisquam...
+                </p>
+              </td>
+              <td class="align-middle text-center text-sm">
+                <span class="badge badge-sm bg-gradient-success">Selesai</span>
+              </td>
+              <!-- <td class="align-middle text-center">
+                <span class="text-secondary text-xs font-weight-bold"
+                  >19/09/17</span
+                >
+              </td> -->
+              <td class="align-middle">
+                <a
+                  href="javascript:;"
+                  class="text-secondary font-weight-bold text-xs"
+                  data-toggle="tooltip"
+                  data-original-title="Detail user"
+                  >Detail</a
+                >
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <div class="d-flex px-2 py-1">
+                  <div>
+                    <img
+                      src="../../assets/img/high.png"
+                      class="avatar avatar-sm me-3"
+                      alt="user4"
+                    />
+                  </div>
+                  <div class="d-flex flex-column justify-content-center">
+                    <h6 class="mb-0 text-sm">Jalan Rusak</h6>
+                    <p class="text-xs text-secondary mb-0">
+                      jalan ini sudah 1 bulan rusak karena sering.....
+                    </p>
+                  </div>
+                </div>
+              </td>
+              <td>
+                <p class="text-xs font-weight-bold mb-0">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  <br />deleniti molestiae doloribus voluptates natus eos ex
+                  quisquam...
+                </p>
+              </td>
+              <td class="align-middle text-center text-sm">
+                <span class="badge badge-sm bg-gradient-success">Selesai</span>
+              </td>
+              <!-- <td class="align-middle text-center">
+                <span class="text-secondary text-xs font-weight-bold"
+                  >24/12/08</span
+                >
+              </td> -->
+              <td class="align-middle">
+                <a
+                  href="javascript:;"
+                  class="text-secondary font-weight-bold text-xs"
+                  data-toggle="tooltip"
+                  data-original-title="Detail user"
+                  >Detail</a
+                >
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <div class="d-flex px-2 py-1">
+                  <div>
+                    <img
+                      src="../../assets/img/high.png"
+                      class="avatar avatar-sm me-3"
+                      alt="user5"
+                    />
+                  </div>
+                  <div class="d-flex flex-column justify-content-center">
+                    <h6 class="mb-0 text-sm">Jalan Rusak</h6>
+                    <p class="text-xs text-secondary mb-0">
+                      jalan ini sudah 1 bulan rusak karena sering.....
+                    </p>
+                  </div>
+                </div>
+              </td>
+              <td>
+                <p class="text-xs font-weight-bold mb-0">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  <br />deleniti molestiae doloribus voluptates natus eos ex
+                  quisquam...
+                </p>
+              </td>
+              <td class="align-middle text-center text-sm">
+                <span class="badge badge-sm bg-gradient-secondary"
+                  >Diproses</span
+                >
+              </td>
+              <!-- <td class="align-middle text-center">
+                <span class="text-secondary text-xs font-weight-bold"
+                  >04/10/21</span
+                >
+              </td> -->
+              <td class="align-middle">
+                <a
+                  href="javascript:;"
+                  class="text-secondary font-weight-bold text-xs"
+                  data-toggle="tooltip"
+                  data-original-title="Detail user"
+                  >Detail</a
+                >
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <div class="d-flex px-2 py-1">
+                  <div>
+                    <img
+                      src="../../assets/img/high.png"
+                      class="avatar avatar-sm me-3"
+                      alt="user6"
+                    />
+                  </div>
+                  <div class="d-flex flex-column justify-content-center">
+                    <h6 class="mb-0 text-sm">Jalan Rusak</h6>
+                    <p class="text-xs text-secondary mb-0">
+                      jalan ini sudah 1 bulan rusak karena sering.....
+                    </p>
+                  </div>
+                </div>
+              </td>
+              <td>
+                <p class="text-xs font-weight-bold mb-0">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  <br />deleniti molestiae doloribus voluptates natus eos ex
+                  quisquam...
+                </p>
+              </td>
+              <td class="align-middle text-center text-sm">
+                <span class="badge badge-sm bg-gradient-secondary"
+                  >Diproses</span
+                >
+              </td>
+              <!-- <td class="align-middle text-center">
+                <span class="text-secondary text-xs font-weight-bold"
+                  >14/09/20</span
+                >
+              </td> -->
+              <td class="align-middle">
+                <a
+                  href="javascript:;"
+                  class="text-secondary font-weight-bold text-xs"
+                  data-toggle="tooltip"
+                  data-original-title="Detail user"
+                  >Detail</a
+                >
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "authors-table",
+};
+</script>
